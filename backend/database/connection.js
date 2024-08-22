c91c51476423e53a4ce453fec0f3b883efa8936d
SuyashShukla0007 <123103079@nitkkr.ac.in>
2024-08-22 23:06:05 +0530

    Update connection.js

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
   async function connectDB() {
-  await mongoose.connect('mongodb://localhost:27017/').then(
+  await mongoose.connect('mongodb+srv://123103079:PwB5tnnu0MS67Jz5@cluster0.u9ece.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(
     () => {
       console.log("Db connected successfully")
     },

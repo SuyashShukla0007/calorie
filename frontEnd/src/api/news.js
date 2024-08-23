3f794385b04756d6b4a243e89d20d643f420250b
SuyashShukla0007 <123103079@nitkkr.ac.in>
2024-08-24 01:19:51 +0530

    Update news.js

--- frontEnd/src/api/news.js
+++ frontEnd/src/api/news.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const news = async () => {
   try {
-    const response = await axios.get('https://calorie-rose.vercel.app/news');
-       console.log(response)
+    const response = await axios.get('https://calorie-rose.vercel.app/api/news');
+       // console.log(response)
     return response.data.json();
  
   } catch (error) {

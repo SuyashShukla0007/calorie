2d721d65e7c79a7e00ff31a9ffd4f8236e62af39
SuyashShukla0007 <123103079@nitkkr.ac.in>
2024-08-24 13:47:56 +0530

    Update Calorie.tsx

--- frontEnd/src/pages/Calorie.tsx
+++ frontEnd/src/pages/Calorie.tsx
@@ -5,16 +5,27 @@ import BarChart from '../components/Chart/Bar';
 import DoughnutChart from '../components/Chart/Doughnut';
 //@ts-ignore
 import { Navigate } from 'react-router-dom';
-
+import {motion} from 'framer-motion'
 interface User {
   firstname: string;
   lastname: string;
   email: string;
 }
 
+
+
+
 const Calorie: React.FC = () => {
   const [userData, setUserData] = useState<User | null>(null);
   const [showSuccessAlert, setShowSuccessAlert] = useState(false);
+
+ 
+
+  const cardVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -49,21 +60,32 @@ const Calorie: React.FC = () => {
   }, [showSuccessAlert]);
 
   return (
-    <div>
+<div> 
       {showSuccessAlert && <SuccessAlert />}
       {userData ? (
+        <motion.div variants={cardVariants}>
         <h1 className='text-4xl font-bold'>Hi, {userData?.firstname} </h1>
+        </motion.div>
       ) : (
         <p>Loading...</p>
       )}
       
       <div className='h-[40vh] w-[30vw] px-10 ml-[2vw] mt-12 bg-black rounded-xl'>
+      <motion.div variants={cardVariants}>
         <BarChart />
+        </motion.div>
       </div>
+      
+      
       <div className='top-[6vh] absolute right-[10vw] rounded-3xl bg-black h-[45vh] w-[25vw] flex items-center justify-center'>
+      <motion.div variants={cardVariants}>
       <DoughnutChart />
+      </motion.div>
       </div>
-    </div>
+      
+    
+      </div>
+
   );
 };
 

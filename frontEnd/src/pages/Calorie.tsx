3b0d573575e8866f952014890d5bc03eedf29a68
SuyashShukla0007 <123103079@nitkkr.ac.in>
2024-08-24 14:13:08 +0530

    Update Calorie.tsx

--- frontEnd/src/pages/Calorie.tsx
+++ frontEnd/src/pages/Calorie.tsx
@@ -1,23 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import SuccessAlert from '../components/accessories/SuccessAlert';
 import BarChart from '../components/Chart/Bar';
 import DoughnutChart from '../components/Chart/Doughnut';
 //@ts-ignore
 import { Navigate } from 'react-router-dom';
 import {motion} from 'framer-motion'
-interface User {
-  firstname: string;
-  lastname: string;
-  email: string;
-}
 
 
 
 
 const Calorie: React.FC = () => {
-  const [userData, setUserData] = useState<User | null>(null);
-  const [showSuccessAlert, setShowSuccessAlert] = useState(false);
 
  
 
@@ -27,50 +18,14 @@ const Calorie: React.FC = () => {
 
   };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const token=localStorage.getItem('auth');
-        const res = await axios.get(' https://calorie-2.onrender.com/api/user',{ 
-          headers:{'Authorization':`Bearer ${token}`},
-          withCredentials: true});
-        setUserData(res.data.user);
-        console.log(res.data.user);
+ 
 
-        const userKey = `${res.data.user.email}_alertShown`;
-        const alertShown = localStorage.getItem(userKey);
 
-        if (!alertShown) {
-          setShowSuccessAlert(true);
-          localStorage.setItem(userKey, 'true');
-        }
-      } catch (err) {
-        console.log("error", err);
-      }
-    };
-
-    fetchData();
-  }, [window.location.href]);
-
-  useEffect(() => {
-    if (showSuccessAlert) {
-      const timer = setTimeout(() => setShowSuccessAlert(false), 5000);
-      return () => clearTimeout(timer);
-    }
-  }, [showSuccessAlert]);
 
   return (
 <div> 
-      {showSuccessAlert && <SuccessAlert />}
-      {userData ? (
-        <motion.div variants={cardVariants}>
-        <h1 className='text-4xl font-bold'>Hi, {userData?.firstname} </h1>
-        </motion.div>
-      ) : (
-        <p>Loading...</p>
-      )}
       
-      <div className='h-[40vh] w-[30vw] px-10 ml-[2vw] mt-12 bg-black rounded-xl'>
+      <div className='h-[40vh] w-[30vw] px-10 ml-[2vw] mt-10 bg-black rounded-xl'>
       <motion.div variants={cardVariants}>
         <BarChart />
         </motion.div>

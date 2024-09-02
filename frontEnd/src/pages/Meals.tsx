f5a80919d81295727eb4e197df370dfc4ce96d00
SuyashShukla0007 <123103079@nitkkr.ac.in>
2024-09-03 00:34:45 +0530

    Update Meals.tsx

--- frontEnd/src/pages/Meals.tsx
+++ frontEnd/src/pages/Meals.tsx
@@ -6,6 +6,7 @@ import { motion } from 'framer-motion'; // Importing framer-motion for animation
 import Loading from '../components/accessories/Loading';
 
 const Meals = () => {
+  
   const { breakFast, dinner, snacks, lunch, calories, fetchCalories, carbohydrates, protein, fat, getMeals } = useMealsContext();
 
   useEffect(() => {
@@ -31,9 +32,8 @@ const Meals = () => {
 
   return (
     <>
+      {!breakFast && <Loading />}
 
-   {!breakFast && <Loading/>}
-      
       <div className="w-[75vw] p-4 lg:p-8">
         <h1 className="text-4xl pl-12 sm:text-5xl md:text-6xl lg:text-6xl font-inknut-antiqua font-heavy mb-8">
           Today's Meal
@@ -60,34 +60,34 @@ const Meals = () => {
         </motion.div>
       </div>
 
-      <div className="w-[75vw] p-4 lg:p-8 flex flex-col lg:flex-row gap-4 lg:gap-8 lg:justify-between lg:items-start"> {/* Added flex-row for large screens and gap between sections */}
+      <div className="w-[75vw] p-4 lg:p-8 flex flex-col lg:flex-row gap-4 lg:gap-8 lg:justify-between lg:items-start">
+        {/* Added flex-row for large screens and gap between sections */}
         <motion.div variants={cardVariants} className="flex flex-col space-y-4 w-full lg:w-1/3">
-          <div className="font-bold text-lg sm:text-xl pl-8">
-            <div className='flex'>
-            <p className="text-blue-800 mb-2">Calories consumed today:</p>
-            <p className="text-black">{(calories ?? 0).toFixed(0)}</p>
+          <div className="font-bold text-lg sm:text-xl pl-8 text-left sm:text-center lg:text-left">
+            <div className="flex">
+              <p className="text-blue-800 mb-2">Calories consumed today:</p>
+              <p className="text-black">{(calories ?? 0).toFixed(0)}</p>
             </div>
           </div>
-          <div className="font-bold text-lg sm:text-xl pl-8">
-          <div className='flex w-[35vw]'>
-            <p className="text-blue-800 mb-2">Carbohydrates consumed today:</p>
-            <p className="text-black">{(carbohydrates ?? 0).toFixed(2)}</p>
+          <div className="font-bold text-lg sm:text-xl pl-8 text-left sm:text-center lg:text-left">
+            <div className="flex w-[35vw]">
+              <p className="text-blue-800 mb-2">Carbohydrates consumed today:</p>
+              <p className="text-black">{(carbohydrates ?? 0).toFixed(2)}</p>
             </div>
-
           </div>
         </motion.div>
 
         <motion.div variants={cardVariants} className="flex flex-col space-y-4 w-full lg:w-1/3">
-          <div className="font-bold text-lg sm:text-xl pl-8">
-          <div className='flex '>
-            <p className="text-blue-800 mb-2">Protein consumed today:</p>
-            <p className="text-black">{(protein ?? 0).toFixed(2)}</p>
+          <div className="font-bold text-lg sm:text-xl pl-8 text-left sm:text-center lg:text-left">
+            <div className="flex">
+              <p className="text-blue-800 mb-2">Protein consumed today:</p>
+              <p className="text-black">{(protein ?? 0).toFixed(2)}</p>
             </div>
           </div>
-          <div className="font-bold text-lg sm:text-xl pl-8">
-          <div className='flex '>
-            <p className="text-blue-800 mb-2">Fat consumed today:</p>
-            <p className="text-black">{(fat ?? 0).toFixed(2)}</p>
+          <div className="font-bold text-lg sm:text-xl pl-8 text-left sm:text-center lg:text-left">
+            <div className="flex">
+              <p className="text-blue-800 mb-2">Fat consumed today:</p>
+              <p className="text-black">{(fat ?? 0).toFixed(2)}</p>
             </div>
           </div>
         </motion.div>

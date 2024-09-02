5c09a47e084caa41806faa273455d168c7963c3b
SuyashShukla0007 <123103079@nitkkr.ac.in>
2024-09-02 23:00:28 +0530

    Update news.js

--- frontEnd/src/api/news.js
+++ frontEnd/src/api/news.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const news = async () => {
   try {
-    const response = await axios.get('https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=4495415c9b6b4982b5cd3b9ebe8ddfd2');
+    const response = await axios.get('https://saurav.tech/NewsAPI/top-headlines/category/health/in.json');
     return response.data; // Return the data directly
 
   } catch (error) {

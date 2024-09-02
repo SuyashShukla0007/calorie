42b8541878a95ba8515bdbf5038d93fa2b2812c5
SuyashShukla0007 <123103079@nitkkr.ac.in>
2024-09-02 23:24:53 +0530

    Update Bar.tsx

--- frontEnd/src/components/Chart/Bar.tsx
+++ frontEnd/src/components/Chart/Bar.tsx
@@ -33,7 +33,7 @@ const BarChart = () => {
  
 
   return (
-    <div className="absolute left-[4vw] h-[40vh] w-[40vw] rounded-xl top-[94vh] bg-black">
+    <div className="absolute left-[4vw] h-[40vh] w-[40vw] top-[58vh] rounded-xl  bg-black xl:hidden">
       <Line
         className="h-60 xl:h-96 w-[40vw]"
         data={{

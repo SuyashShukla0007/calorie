96189891b6ac99cf89dce34763a7901f7cc6ed3e
SuyashShukla0007 <123103079@nitkkr.ac.in>
2024-09-03 16:35:57 +0530

    Update Loading.tsx

--- frontEnd/src/components/accessories/Loading.tsx
+++ frontEnd/src/components/accessories/Loading.tsx
@@ -3,7 +3,7 @@ import './LoadingDots.css'; // Import the CSS file for the wave animation
 
 const LoadingDots: React.FC = () => {
   return (
-    <div className="flex w-[100vw]  justify-center top-0 left-[0vw] absolute lg:w-[76vw] m-0 p-0 items-center h-screen bg-black">
+    <div className="flex w-[100vw]  justify-center top-0 left-[0vw] absolute lg:w-[76vw] lg:ml-[24vw] m-0 p-0 items-center h-screen bg-black">
       <div className="flex space-x-2">
         <div className="w-4 h-4 bg-white rounded-full dot"></div>
         <div className="w-4 h-4 bg-white rounded-full dot"></div>
